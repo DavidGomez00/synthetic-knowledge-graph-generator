2623207f0d83ee63eb1cf973afac59f17d22b051
David Gómez <dvdgmzrtz@gmail.com>
2026-09-10 17:43:46 +0200

    Deleted simple mario graphs, added office simplest case

--- src/skgg/cli/upload.py
+++ src/skgg/cli/upload.py
@@ -6,12 +6,10 @@ from pathlib import Path
 
 from skgg.config import RunConfig
 from skgg.core.queries import get_triple_count, initialize_graph
-from skgg.core.rules import parse_rule_set
-from skgg.engine.completion import complete_graph
 from skgg.utils import create_sparql_client, get_term_mapping, setup_logging
 
 ### EDIT THIS PATH   vvv
-graph_config = Path("configurations/mario.json")
+graph_config = Path("configurations/office_pygraft.json")
 config = RunConfig.from_json(graph_config)
 
 setup_logging(level=config.logging.level)
@@ -26,11 +24,11 @@ term_mapping = get_term_mapping(
     default_namespace=config.graph.namespace,
 )
 
-rules = parse_rule_set(
-    rules_file=input_dir / config.rules.rules_file,
-    term_mapping=term_mapping,
-    pca_threshold=config.rules.pca_threshold,
-)
+# rules = parse_rule_set(
+#     rules_file=input_dir / config.rules.rules_file,
+#     term_mapping=term_mapping,
+#     pca_threshold=config.rules.pca_threshold,
+# )
 
 # SPARQL client
 client = create_sparql_client(config)
@@ -47,20 +45,20 @@ initialize_graph(
 base_count = get_triple_count(client, base_uri)
 logger.info("Inserted base graph to <%s> with %d triples.", base_uri, base_count)
 
-logger.info("Starting Graph Completion")
+# logger.info("Starting Graph Completion")
 
-# Complete graph
-complete_graph(
-    client=client,
-    rules=rules,
-    term_mapping=term_mapping,
-    base_uri=base_uri,
-    complete_uri=complete_uri,
-    chunk_size=config.db_config.chunk_size,
-)
+# # Complete graph
+# complete_graph(
+#     client=client,
+#     rules=rules,
+#     term_mapping=term_mapping,
+#     base_uri=base_uri,
+#     complete_uri=complete_uri,
+#     chunk_size=config.db_config.chunk_size,
+# )
 
-logger.info(
-    "Complete graph in <%s> has %d triples.",
-    complete_uri,
-    get_triple_count(client, complete_uri),
-)
+# logger.info(
+#     "Complete graph in <%s> has %d triples.",
+#     complete_uri,
+#     get_triple_count(client, complete_uri),
+# )
